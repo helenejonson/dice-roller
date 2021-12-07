3bbccbfc1e73e05f54742bc3e3e109621d411c14
heleneyj <heleneyj@stud.ntnu.no>
2021-12-07 21:10:46 +0100

    Add project start

--- app/src/main/java/com/example/dice_roller/MainActivity.kt
+++ app/src/main/java/com/example/dice_roller/MainActivity.kt
@@ -1,11 +1,51 @@
 package com.example.dice_roller
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.CompoundButton
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import kotlin.random.Random
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var rollBtn : Button
+    lateinit var adv : CheckBox
+    private lateinit var dis : CheckBox
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        rollBtn = findViewById(R.id.rollbtn)
+        rollBtn.setOnClickListener(listener)
+        //dis.setOnCheckedChangeListener(checkChange)
+    }
+
+    private val listener= View.OnClickListener { view ->
+        when (view.id) {
+            R.id.rollbtn -> {
+                val roll = Random.nextInt(0,20) + 1
+                val rollView = findViewById<TextView>(R.id.mainRoll)
+                val discard = findViewById<TextView>(R.id.second)
+                adv = findViewById(R.id.advantage)
+                dis = findViewById(R.id.disadvantage)
+                if (adv.isChecked || dis.isChecked) {
+                    val roll2 = Random.nextInt(0,20) + 1
+
+                    if(adv.isChecked){
+                        rollView.text = maxOf(roll, roll2).toString()
+                        discard.text = minOf(roll,roll2).toString()
+                    }else{
+                        rollView.text = minOf(roll, roll2).toString()
+                        discard.text = maxOf(roll,roll2).toString()
+                    }
+                }else {
+                    discard.text = ""
+                    rollView.text = roll.toString();
+                }
+            }
+        }
     }
 }
